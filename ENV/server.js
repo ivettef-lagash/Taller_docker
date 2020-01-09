9618125bfed8729db4c862f25b987937a030340a
ivettef <56972785+ivettef@users.noreply.github.com>
2020-01-09 19:09:11 -0300

    variables de entorno ejemplo

--- ENV/server.js
+++ ENV/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-let { HOST, PORT, ENVIROMENT } = process.env;
+let { HOST, PORT, ENVIROMENTS } = process.env;
 
 const app = express();
 
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 
 app.get("/", (req, res) => {
   res.send(
-    `API construida para Lagash University se encuentra arriba en ambiente ${ENVIROMENT ||
+    `API construida para Lagash University se encuentra arriba en ambiente ${ENVIROMENTS ||
       "No definido"}`
   );
 });
